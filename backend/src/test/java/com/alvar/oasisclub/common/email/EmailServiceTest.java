b308a8c56495f332a458f6c905cedfbdea27b8a8
Alvaro <alvaro.alvaro601@gmail.com>
2026-02-27 10:46:00 +0000

    estructura inicial de la clase EmailServiceTest

---
+++ backend/src/test/java/com/alvar/oasisclub/common/email/EmailServiceTest.java
@@ -0,0 +1,25 @@
+package com.alvar.oasisclub.common.email;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.alvar.oasisclub.common.config.MailSenderProperties;
+import jakarta.mail.Session;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import java.util.Properties;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mail.javamail.JavaMailSender;
+
+@ExtendWith(MockitoExtension.class)
+class EmailServiceTest {
+}
