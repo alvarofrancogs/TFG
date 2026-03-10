6d0a5dd67ec8a670782b5a10d0f07a895e3bdf3f
Alvaro <alvaro.alvaro601@gmail.com>
2026-03-10 15:17:00 +0000

    entidad de EventRegistrationEntity mapeada

---
+++ backend/src/main/java/com/alvar/oasisclub/events/entity/EventRegistrationEntity.java
@@ -0,0 +1,42 @@
+package com.alvar.oasisclub.events.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "event_registrations")
+public class EventRegistrationEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "event_id", nullable = false)
+  private EventEntity event;
+
+  @Column(name = "client_id", nullable = false)
+  private UUID clientId;
+
+  @Column(name = "created_at", nullable = false)
+  private LocalDateTime createdAt;
+}
