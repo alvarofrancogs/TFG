1c1946334cba8e6c3caee3cd35485a5e513b9eb2
Alvaro <alvaro.alvaro601@gmail.com>
2026-03-18 10:43:00 +0100

    reservation response dto

---
+++ backend/src/main/java/com/alvar/oasisclub/reservations/dto/ReservationResponse.java
@@ -0,0 +1,19 @@
+package com.alvar.oasisclub.reservations.dto;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import lombok.Data;
+
+@Data
+public class ReservationResponse {
+  private String id;
+  private String clientId;
+  private String userName;
+  private String sport;
+  private String court;
+  private LocalDate date;
+  private LocalTime time;
+  private String status;
+  private String stripeSessionId;
+}
+
