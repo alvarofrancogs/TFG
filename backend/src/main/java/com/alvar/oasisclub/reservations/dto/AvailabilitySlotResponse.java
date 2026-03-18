202219e531f5da1c9ca4c97d125228b775975df6
Alvaro <alvaro.alvaro601@gmail.com>
2026-03-18 11:19:00 +0100

    availability slot response

---
+++ backend/src/main/java/com/alvar/oasisclub/reservations/dto/AvailabilitySlotResponse.java
@@ -0,0 +1,12 @@
+package com.alvar.oasisclub.reservations.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AvailabilitySlotResponse {
+  private String time;
+  private boolean available;
+}
+
