92938c3f5497e9d24f63b50219d4d8df02c3a966
Alvaro <alvaro.alvaro601@gmail.com>
2026-03-27 10:39:00 +0100

    profile reservation response

---
+++ backend/src/main/java/com/alvar/oasisclub/profile/dto/ProfileReservationResponse.java
@@ -0,0 +1,16 @@
+package com.alvar.oasisclub.profile.dto;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import lombok.Data;
+
+@Data
+public class ProfileReservationResponse {
+  private String id;
+  private String sport;
+  private String court;
+  private LocalDate date;
+  private LocalTime time;
+  private String status;
+}
+
