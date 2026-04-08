ec84940f04a3ec39a711a80357a831e195029d6e
Alvaro <alvaro.alvaro601@gmail.com>
2026-04-08 17:43:00 +0200

    gym routine day entity

---
+++ backend/src/main/java/com/alvar/oasisclub/gym/entity/GymRoutineDayEntity.java
@@ -0,0 +1,38 @@
+package com.alvar.oasisclub.gym.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "gym_routine_days")
+public class GymRoutineDayEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  @Column(name = "client_id", nullable = false)
+  private UUID clientId;
+
+  @Column(name = "day_order", nullable = false)
+  private Integer dayOrder;
+
+  @Column(nullable = false, length = 120)
+  private String name;
+}
+
