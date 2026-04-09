6f407eec8b31a3ca4a42c76f35dc744325fe5808
Alvaro <alvaro.alvaro601@gmail.com>
2026-04-09 09:12:00 +0000

    dto de CreateReservationRequest para las peticiones

---
+++ backend/src/main/java/com/alvar/oasisclub/reservations/dto/CreateReservationRequest.java
@@ -0,0 +1,31 @@
+package com.alvar.oasisclub.reservations.dto;
+
+import com.alvar.oasisclub.reservations.entity.SportType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import lombok.Data;
+
+@Data
+public class CreateReservationRequest {
+
+  private String clientId;
+
+  @NotBlank
+  private String userName;
+
+  @NotNull
+  private SportType sport;
+
+  @NotNull
+  private java.util.UUID courtId;
+
+  @NotNull
+  private LocalDate date;
+
+  @NotNull
+  private LocalTime time;
+}
+
+
