fdfff486dcee513b1e02f3cc269911fc9565154d
Emilio <emiliobeniloza@gmail.com>
2026-04-14 17:55:00 +0200

    quienes somos ts y html

---
+++ frontend/src/app/componentes/quienes-somos/quienes-somos.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+
+@Component({
+  selector: 'app-quienes-somos',
+  imports: [NgOptimizedImage],
+  templateUrl: './quienes-somos.html',
+  styleUrl: './quienes-somos.css',
+})
+export class QuienesSomos {
+
+}
