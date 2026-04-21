638974993b718bb4e8028ed731aecb6021d47ef4
Alvaro <alvaro.alvaro601@gmail.com>
2026-04-21 18:41:00 +0200

    client email already exists

---
+++ backend/src/main/java/com/alvar/oasisclub/clients/exception/ClientEmailAlreadyExistsException.java
@@ -0,0 +1,9 @@
+package com.alvar.oasisclub.clients.exception;
+
+public class ClientEmailAlreadyExistsException extends RuntimeException {
+
+  public ClientEmailAlreadyExistsException(String message) {
+    super(message);
+  }
+}
+
