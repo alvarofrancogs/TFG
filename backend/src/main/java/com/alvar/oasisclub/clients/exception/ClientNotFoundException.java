b8fa05071e12984f963759f8a0bf7f892d8b87d6
Alvaro <alvaro.alvaro601@gmail.com>
2026-04-21 18:03:00 +0200

    client not found exception

---
+++ backend/src/main/java/com/alvar/oasisclub/clients/exception/ClientNotFoundException.java
@@ -0,0 +1,9 @@
+package com.alvar.oasisclub.clients.exception;
+
+public class ClientNotFoundException extends RuntimeException {
+
+  public ClientNotFoundException(String message) {
+    super(message);
+  }
+}
+
