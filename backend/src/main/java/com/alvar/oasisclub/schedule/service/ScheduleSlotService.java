3b2fa4a6bab9089d9a72e222902f491253a3516d
Alvaro <alvaro.alvaro601@gmail.com>
2026-04-25 14:49:00 +0000

    estructura inicial de la clase ScheduleSlotService

---
+++ backend/src/main/java/com/alvar/oasisclub/schedule/service/ScheduleSlotService.java
@@ -0,0 +1,20 @@
+package com.alvar.oasisclub.schedule.service;
+
+import com.alvar.oasisclub.reservations.entity.ReservationEntity;
+import com.alvar.oasisclub.reservations.entity.ReservationStatus;
+import com.alvar.oasisclub.reservations.repository.ReservationRepository;
+import com.alvar.oasisclub.reservations.service.ReservationService;
+import com.alvar.oasisclub.schedule.entity.ScheduleSlotEntity;
+import com.alvar.oasisclub.schedule.repository.ScheduleSlotRepository;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ScheduleSlotService {
+}
