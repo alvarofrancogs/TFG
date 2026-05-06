0373a0226dd70bbca2d70881e7ca298282813e5b
Emilio <emiliobeniloza@gmail.com>
2026-05-06 10:53:00 +0200

    pago completado ts y html

---
+++ frontend/src/app/componentes/pago-completado/pago-completado.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-pago-completado',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './pago-completado.component.html',
+})
+export class PagoCompletadoComponent {
+}
