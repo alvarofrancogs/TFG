6a682d67a8692657094f50d5dd02a1eb5b2cd185
Emilio <emiliobeniloza@gmail.com>
2026-05-13 12:44:00 +0000

    lógica typescript del componente tarifas.component

---
+++ frontend/src/app/componentes/tarifas/tarifas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-tarifas',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './tarifas.component.html',
+  styleUrl: './tarifas.component.css',
+})
+export class TarifasComponent {}
