efa6c6c67444b80cef4cfada21ee0b2e43b2f0b0
Alvaro <alvaro.alvaro601@gmail.com>
2026-05-20 08:44:00 +0000

    estructura inicial de la clase JwtServiceTest

---
+++ backend/src/test/java/com/alvar/oasisclub/auth/JwtServiceTest.java
@@ -0,0 +1,15 @@
+package com.alvar.oasisclub.auth;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.alvar.oasisclub.auth.security.JwtService;
+import java.lang.reflect.Field;
+import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class JwtServiceTest {
+}
